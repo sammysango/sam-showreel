ac28a3b22c636673c2ac7d2900b732914374f085
sammysango <136692631+sammysango@users.noreply.github.com>
2024-03-30 14:54:41 +0000

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    settings: {
-      react: {
-        version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+  },
+  overrides: [
+    {
+      files: ["webpack.config.js", ".eslintrc.{js,cjs}"], // Now includes webpack.config.js
+      env: {
+        node: true, // This applies Node.js environment settings to webpack.config.js and ESLint config files
+      },
+      parserOptions: {
+        sourceType: "script",
       },
     },
-    overrides: [
-      {
-        files: ["webpack.config.js", ".eslintrc.{js,cjs}"], // Now includes webpack.config.js
-        env: {
-          node: true, // This applies Node.js environment settings to webpack.config.js and ESLint config files
-        },
-        parserOptions: {
-          sourceType: "script",
-        },
-      },
-    ],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {},
-  };  
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './css/style.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./css/style.css";
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container); // Create a root.
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
