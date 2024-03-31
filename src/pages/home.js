2bf10c68b6ddbb547c7132b0bfac9f6e2961586c
sammysango <136692631+sammysango@users.noreply.github.com>
2024-03-31 02:15:51 +0100

    various navbar style cleanup and responsiveness

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,14 @@ const HomePage = () => {
     <div>
       <h1>Home Page</h1>
       <p>This is the Home Page. Add your content here.</p>
+      {/* Add placeholder content to allow for scrolling */}
+      {Array.from({ length: 50 }, (_, index) => (
+        <p key={index}>
+          Placeholder content paragraph {index + 1}. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+        </p>
+      ))}
     </div>
   );
 };
