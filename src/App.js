7ac053c82a0f42bc9e3a942633aacd124c94e3d2
sammysango <136692631+sammysango@users.noreply.github.com>
2024-03-31 02:55:34 +0100

    fix test 2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React from "react";
-import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useTheme } from "./ThemeContext";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/home";
-import AboutPage from "./pages/about";
-import PortfolioPage from "./pages/portfolio";
-import ContactPage from "./pages/contact";
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Import HashRouter
+import { useTheme } from './ThemeContext';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import HomePage from './pages/home';
+import AboutPage from './pages/about';
+import PortfolioPage from './pages/portfolio';
+import ContactPage from './pages/contact';
 
 const App = () => {
-  const { theme, colorScheme } = useTheme();
+    const { theme, colorScheme } = useTheme();
+   
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+        document.body.setAttribute('data-color-scheme', colorScheme);
+    }, [theme, colorScheme]);
 
-  useEffect(() => {
-    document.body.setAttribute("data-theme", theme);
-    document.body.setAttribute("data-color-scheme", colorScheme);
-  }, [theme, colorScheme]);
-
-  return (
-    <div data-theme={theme} data-color-scheme={colorScheme}>
-      <Router basename={window.location.pathname}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+    return (
+        <div data-theme={theme} data-color-scheme={colorScheme}>
+            <Router> {/* Use HashRouter */}
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/portfolio" element={<PortfolioPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
 };
 
 export default App;
