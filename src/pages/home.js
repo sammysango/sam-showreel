bffa166284fa55c9b60018d16d085386fa049b8c
sammysango <136692631+sammysango@users.noreply.github.com>
2024-04-03 19:14:57 +0100

    setting up banner component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "./home.css";
+import BannerComponent from "../components/HomeBanner/HomeBanner";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="module feature">Featured Work Stream</div>
-      <div className="module banner">Banner</div>
+      <div className="module banner">
+        <BannerComponent />
+      </div>
       <div className="module sideModule">Side Module</div>
       <div className="modulesUnderBanner">
         <div className="module module1">Module 1</div>
         <div className="module module2">Module 2</div>
       </div>
-      <div className="module module3">Module 3</div> {/* New Module 3 */}
+      <div className="module module3">Module 3</div>
     </div>
   );
 };
