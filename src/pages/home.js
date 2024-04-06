58e85c3a086d4b5ff3263b0f1863b0f014a2a2db
sammysango <136692631+sammysango@users.noreply.github.com>
2024-04-07 00:56:32 +0100

    scrolling brands/collabs component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import BannerComponent from "../components/HomeBanner/HomeBanner";
 import SideSlideshow from "../components/SideSlideshow/SideSlideshow";
+import BrandScroll from "../components/BrandScroll/BrandScroll";
 
 const HomePage = () => {
   return (
@@ -18,6 +19,9 @@ const HomePage = () => {
         <div className="module module2">Module 2</div>
       </div>
       <div className="module module3">Module 3</div>
+      <div className="module bottomModule">
+        <BrandScroll />
+      </div>
     </div>
   );
 };
