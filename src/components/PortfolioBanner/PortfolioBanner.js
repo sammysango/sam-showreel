64dce48fe7145b7dc5bc265313ffac9ebeca6dfd
sammysango <136692631+sammysango@users.noreply.github.com>
2024-04-13 16:19:23 +0100

    portfolio banner improvements

--- src/components/PortfolioBanner/PortfolioBanner.js
+++ src/components/PortfolioBanner/PortfolioBanner.js
@@ -11,32 +11,28 @@ const PortfolioBanner = ({ bgColor = "teal", textColor = "white" }) => {
       const rect = grid.getBoundingClientRect();
       const numCols = 10;
       const numRows = 10;
-  
-      // Calculate mouse position relative to the visible part of the grid
+
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
-  
-      // Calculate the hovered cell based on mouse position
+
       const col = Math.floor((numCols * x) / rect.width);
       const row = Math.floor((numRows * y) / rect.height);
-  
+
       setHoverCell({ row, col });
     };
-  
-    const grid = gridRef.current;
-    grid.addEventListener('mousemove', handleMouseMove);
-  
+
+    gridRef.current.addEventListener("mousemove", handleMouseMove);
     return () => {
-      grid.removeEventListener('mousemove', handleMouseMove);
+      gridRef.current.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);  
+  }, []);
 
   const calculateRadius = (row, col) => {
     const dx = Math.abs(hoverCell.col - col);
     const dy = Math.abs(hoverCell.row - row);
     const distance = Math.sqrt(dx * dx + dy * dy);
-    const maxRadiusEffect = 50; // The maximum radius effect (in percent)
-    const effectSpread = 10; // This determines how quickly the effect spreads; smaller values mean a wider spread
+    const maxRadiusEffect = 50;
+    const effectSpread = 15;
     return Math.max(0, maxRadiusEffect - distance * effectSpread);
   };
 
@@ -54,10 +50,23 @@ const PortfolioBanner = ({ bgColor = "teal", textColor = "white" }) => {
             <div
               key={index}
               className={styles.square}
-              style={{ borderRadius: `${radius}%`, backgroundColor: bgColor }}
+              style={{ backgroundColor: bgColor, borderRadius: `${radius}%` }}
             />
           );
         })}
+      </div>
+      <div className={styles.arrowContainer}>
+        {/* Ensure SVG is visible for debugging */}
+        <svg
+          className={styles.arrowSvg}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 15.692 27.334"
+        >
+          <path
+            d="M2,27.334c-.512,0-1.024-.195-1.414-.586-.781-.781-.781-2.047,0-2.828l10.277-10.277L.586,3.365c-.781-.781-.781-2.047,0-2.828C1.367-.245,2.633-.245,3.414.537l11.692,11.691c.375.375.586.884.586,1.414s-.211,1.039-.586,1.414L3.414,26.748c-.39.391-.902.586-1.414.586Z"
+            fill="currentColor"
+          />
+        </svg>
       </div>
     </div>
   );
